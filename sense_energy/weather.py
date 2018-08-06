48527f7ac59710e0385190be04a77cca141eb162
firestormo <42076498+firestormo@users.noreply.github.com>
2018-08-06 13:15:49 -0600

    Create weather.py
    used to pull weather data for inclusion in pvoutput u/l of data

---
+++ sense_energy/weather.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import feedparser
+
+
+#Set Variables to ensure we start fresh
+min_temp = ''
+max_temp = ''
+
+#OpenweatherMap Settings
+owm_api = '' #http://openweathermap.org/appid#get
+owm_cityid = '' #Search for city ID here: http://openweathermap.org/help/city_list.txt. ID is the first column
+owm_unit = 'metric' #metric or imperial
+
+#Open Weather Map API URL
+weatherURL = feedparser.parse ('http://api.openweathermap.org/data/2.5/weather?id=%s&appid=%s&mode=xml&units=%s' % (owm_cityid, owm_api, owm_unit))
+
+#Get Yahoo Weather Code, min temp, max temp and weather text summary
+min_temp = weatherURL.feed.temperature.get('min')
+max_temp = weatherURL.feed.temperature.get('max')
+forecast_text = weatherURL.feed.weather.get('value')
+
