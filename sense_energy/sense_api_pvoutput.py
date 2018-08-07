65bafd590cafd310a2eab2599463280010a0fc3e
firestormo <42076498+firestormo@users.noreply.github.com>
2018-08-07 07:53:09 -0600

    Update sense_api_pvoutput.py
    re-work and cleanup, now import sense_api info instead of copying code to this file. round temp to 1st decimal point. cleanup curl URL for PVOutput

--- sense_energy/sense_api_pvoutput.py
+++ sense_energy/sense_api_pvoutput.py
@@ -1,245 +1,25 @@
 import sys, subprocess
-import json
-import requests
 from datetime import datetime, date, time
-from websocket import create_connection
+from sense_api import Senseable
 from weather import current_temp, min_temp, max_temp, forecast_text
 
-API_URL = 'https://api.sense.com/apiservice/api/v1/'
-API_TIMEOUT = 1
-WSS_TIMEOUT = 1
+DATE = datetime.now().strftime("%Y%m%d")
+TIME = datetime.now().strftime("%H:%M")
+username = 'senseusername'
+password = 'sensepass'
 PVOutputURL = 'https://pvoutput.org/service/r2/addstatus.jsp'
 PVOutputAPI = ''
 PVOutputSID = ''
-date = datetime.now().strftime("%Y%m%d")
-time = datetime.now().strftime("%H:%M")
 
+sense = Senseable(username,password)
+active_power = str(sense.active_power).split('.')[0]
+active_solar_power = str(sense.active_solar_power).split('.')[0]
+active_voltage_split = str(sense.active_voltage).split(' ')[0]
+active_voltage_strip = str(active_voltage_split).strip('[ ,')
+active_voltage = active_voltage_strip[:5]
+temp = round(float(current_temp),1)
 
-# for the last hour, day, week, month, or year
-valid_scales = ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR']
+PVOutputCURL = """curl -d "d={}" -d "t={}" -d "v4={}" -d "v2={}" -d "v5={}" -d "v6={}" -H "X-Pvoutput-APIkey:{}" -H "X-Pvoutput-SystemId:{}" {}""".format(DATE, TIME, active_power, active_solar_power, temp, active_voltage,PVOutputAPI, PVOutputSID, PVOutputURL)
 
-class Senseable(object):
-
-    def __init__(self, username, password):
-        auth_data = {
-            "email": username,
-            "password": password
-        }
-
-        # Create session
-        self.s = requests.session()
-        self._realtime = None
-        self._devices = []
-        self._trend_data = {}
-        for scale in valid_scales: self._trend_data[scale] = {}
-
-        # Get auth token
-        try:
-            response = self.s.post(API_URL+'authenticate', auth_data, timeout=API_TIMEOUT)
-        except Exception as e:
-            raise Exception('Connection failure: %s' % e)
-
-        # check for 200 return
-        if response.status_code != 200:
-            raise Exception("Please check username and password. API Return Code: %s" % response.status_code)
-
-        # Build out some common variables
-        self.sense_access_token = response.json()['access_token']
-        self.sense_user_id = response.json()['user_id']
-        self.sense_monitor_id = response.json()['monitors'][0]['id']
-
-        # create the auth header
-        self.headers = {'Authorization': 'bearer {}'.format(self.sense_access_token)}
-
-    @property
-    def devices(self):
-        """Return devices."""
-        return self._devices
-
-    def get_realtime(self):
-        ws = create_connection("wss://clientrt.sense.com/monitors/%s/realtimefeed?access_token=%s" %
-                               (self.sense_monitor_id, self.sense_access_token),
-                               timeout=WSS_TIMEOUT)
-        for i in range(5): # hello, features, [updates,] data
-            result = json.loads(ws.recv())
-            if result.get('type') == 'realtime_update':
-                self._realtime = result['payload']
-                return self._realtime
-
-    @property
-    def active_power(self):
-        if not self._realtime: self.get_realtime()
-        return self._realtime.get('w', 0)
-
-    @property
-    def active_solar_power(self):
-        if not self._realtime: self.get_realtime()
-        return self._realtime.get('solar_w', 0)
-
-    @property
-    def active_voltage(self):
-        if not self._realtime: self.get_realtime()
-        return self._realtime.get('voltage', 0)
- 
-    @property
-    def active_frequency(self):
-       if not self._realtime: self.get_realtime()
-       return self._realtime.get('hz', 0)
-   
-    @property
-    def daily_usage(self):
-        return self.get_trend('DAY', False)
-
-    @property
-    def daily_production(self):
-        return self.get_trend('DAY', True)
-    
-    @property
-    def weekly_usage(self):
-        # Add today's usage
-        return self.get_trend('WEEK', False)
-
-    @property
-    def weekly_production(self):
-        # Add today's production
-        return self.get_trend('WEEK', True)
-    
-    @property
-    def monthly_usage(self):
-        # Add today's usage
-        return self.get_trend('MONTH', False)
-
-    @property
-    def monthly_production(self):
-        # Add today's production
-        return self.get_trend('MONTH', True)
-    
-    @property
-    def yearly_usage(self):
-        # Add this month's usage
-        return self.get_trend('YEAR', False)
-
-    @property
-    def yeary_production(self):
-        # Add this month's production
-        return self.get_trend('YEAR', True)
-
-    @property
-    def active_devices(self):
-        if not self._realtime: self.get_realtime()
-        return [d['name'] for d in self._realtime.get('devices', {})]
-
-    def get_trend(self, scale, is_production):
-        key = "production" if is_production else "consumption"
-        if not self._trend_data[scale]: self.get_trend_data(scale)         
-        if key not in self._trend_data[scale]: return 0
-        total = self._trend_data[scale][key].get('total', 0)
-        if scale == 'WEEK' or scale == 'MONTH':
-            return total + self.get_trend('DAY', is_production)
-        if scale == 'YEAR':
-            return total + self.get_trend('MONTH', is_production)
-        return total
-
-    def get_discovered_device_names(self):
-        # lots more info in here to be parsed out
-        response = self.s.get(API_URL + 'app/monitors/%s/devices' %
-                              self.sense_monitor_id,
-                              headers=self.headers, timeout=API_TIMEOUT)
-        self._devices = [entry['name'] for entry in response.json()]
-        return self._devices
-
-    def get_discovered_device_data(self):
-        response = self.s.get(API_URL + 'monitors/%s/devices' %
-                              self.sense_monitor_id,
-                              headers=self.headers, timeout=API_TIMEOUT)
-        return response.json()
-
-    def always_on_info(self):
-        # Always on info - pretty generic similar to the web page
-        response = self.s.get(API_URL + 'app/monitors/%s/devices/always_on' %
-                              self.sense_monitor_id,
-                              headers=self.headers, timeout=API_TIMEOUT)
-        return response.json()
-
-    def get_monitor_info(self):
-        # View info on your monitor & device detection status
-        response = self.s.get(API_URL + 'app/monitors/%s/status' %
-                              self.sense_monitor_id,
-                              headers=self.headers, timeout=API_TIMEOUT)
-        return response.json()
-
-    def get_device_info(self, device_id):
-        # Get specific informaton about a device
-        response = self.s.get(API_URL + 'app/monitors/%s/devices/%s' %
-                              (self.sense_monitor_id, device_id),
-                              headers=self.headers, timeout=API_TIMEOUT)
-        return response.json()
-
-    def get_notification_preferences(self):
-        # Get notification preferences
-        payload = {'monitor_id': '%s' % self.sense_monitor_id}
-        response = self.s.get(API_URL + 'users/%s/notifications' %
-                              self.sense_user_id,
-                              headers=self.headers, timeout=API_TIMEOUT,
-                              data=payload)
-        return response.json()
-    
-    def get_trend_data(self, scale):
-        if scale.upper() not in valid_scales:
-            raise Exception("%s not a valid scale" % scale)
-        t = datetime.now().replace(hour=12)
-        response = self.s.get(API_URL + 'app/history/trends?monitor_id=%s&scale=%s&start=%s' %
-                              (self.sense_monitor_id, scale, t.isoformat()),
-                              headers=self.headers, timeout=API_TIMEOUT)
-        self._trend_data[scale] = response.json()
-
-    def update_trend_data(self):
-        for scale in valid_scales:
-            self.get_trend_data(scale)
-
-    def get_all_usage_data(self):
-        payload = {'n_items': 30}
-        # lots of info in here to be parsed out
-        response = self.s.get(API_URL + 'users/%s/timeline' %
-                              self.sense_user_id,
-                              headers=self.headers, timeout=API_TIMEOUT,
-                              data=payload)
-        return response.json()
-
-
-if __name__ == "__main__":
-    import pprint
-    import getpass
-
-
-
-    # collect authn data
-    #username = input("Please enter you Sense username (email address): ")
-    #password = getpass.getpass("Please enter your Sense password: ")
-    username = 'senseuser'
-    password = 'sensepass'
-    sense = Senseable(username, password)
-   
-    active_power = str(sense.active_power).split('.')[0]
-    active_solar_power = str(sense.active_solar_power).split('.')[0]
-    active_voltage_split = str(sense.active_voltage).split('.')[0]
-    active_voltage_strip = str(active_voltage_split).strip('[ ,')
-    active_voltage = active_voltage_strip[:5]
-
-    print ("Active:", sense.active_power, "W")
-    print ("Active Solar:", sense.active_solar_power, "W")
-    print ("Active Devices:", ", ".join(sense.active_devices))
-    print ("Active Voltage:", sense.active_voltage, "V")
-    print ("Active Voltage Trimmed:", sense.active_voltage[0], "V")
-    print ("Active Frequency:", sense.active_frequency, "Hz")
-    print ("Active Power Trimmed:", str(sense.active_power).split('.')[0], "W")
-
-# ---------------- pvoutput
-
-PVOutputCURL1 = """curl -d "d={}" -d "t={}" -d "v4={}" -d "v2={}" -d "v5={}" -d "v6={}" """.format(date, time, active_power, active_solar_power, current_temp, active_voltage)
-PVOutputCURL2 = """-H "X-Pvoutput-APIkey:{}" -H "X-Pvoutput-SystemId:{}" {}""".format(PVOutputAPI, PVOutputSID, PVOutputURL)
-
-PVOutputCURL = PVOutputCURL1+PVOutputCURL2
-AddStatus = (PVOutputCURL)
-subprocess.call(AddStatus, shell=True)
+subprocess.call(PVOutputCURL, shell=True)
 print()
