2a450dfdaa91ce2871dd6567ea4911ced185d241
Keilin <trumpetgod@gmail.com>
2018-09-04 19:57:52 -0400

    0.4.2 Update

--- sense_energy/__init__.py
+++ sense_energy/__init__.py
@@ -1,3 +1,3 @@
 from .sense_api import *
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-  name = 'sense_energy',
-  packages = ['sense_energy'], 
-  install_requires=[
-      'requests',
-      'websocket-client',
-  ],
-  version = '0.4.1',
-  description = 'API for the Sense Energy Monitor',
-  author = 'scottbonline',
-  author_email = 'scottbonline@gmail.com',
-  url = 'https://github.com/scottbonline/sense',
-  download_url = 'https://github.com/scottbonline/sense/archive/0.4.1.tar.gz', 
-  keywords = ['sense', 'energy', 'api', 'pytest'], 
-  classifiers = [],
+    name = 'sense_energy',
+    packages = ['sense_energy'], 
+    install_requires=[
+        'requests',
+        'websocket-client',
+    ],
+    version = '0.4.2',
+    description = 'API for the Sense Energy Monitor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'scottbonline',
+    author_email = 'scottbonline@gmail.com',
+    url = 'https://github.com/scottbonline/sense',
+    download_url = 'https://github.com/scottbonline/sense/archive/0.4.2.tar.gz', 
+    keywords = ['sense', 'energy', 'api', 'pytest'], 
+    classifiers = [],
 )
