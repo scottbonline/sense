936cb529e844b377a8fba66011d0238f78ca4380
Keilin <TrumpetGod@gmail.com>
2022-02-12 12:42:37 -0500

    Version 0.10.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'websockets;python_version>="3.5"',
         'aiohttp;python_version>="3.5"',
     ], 
-    version = '0.9.6',
+    version = '0.10.0',
     description = 'API for the Sense Energy Monitor',
     long_description=long_description,
     long_description_content_type="text/markdown",
