a6f86254caf8b0de9b4dcad57b7eb65282a18a56
kbickar <TrumpetGod@gmail.com>
2023-01-05 22:42:52 -0500

    Fix syntax

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.11.1',
+version = '0.11.1'
 setup(
     name = 'sense_energy',
     packages = ['sense_energy'], 
